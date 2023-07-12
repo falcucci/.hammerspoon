a70041dcd93daf5f98137b05516f95bcdbec8cad
Alexsander Falcucci <alexsander.santos@luizalabs.com>
2023-07-12 14:32:01 +0200

    add firefox configuration
    adds configuration code to allow toggling firefox nightly on/off with a hotkey. when the hotkey is pressed, the application will be either opened, hidden, or brought to the front depending on its current state.
    
    ```diff
    diff --git a/init.lua b/init.lua
    index 221177a21abf..c5a1421ccc43 100644
    --- a/init.lua
    +++ b/init.lua
    @@ -94,3 +94,36 @@ hs.hotkey.bind({"ctrl", "shift"}, "a", function()
         app = hs.application.get("safari")
       end
     end)
    +
    +-----------------------------
    +--  firefox configuration  --
    +-----------------------------
    +hs.hotkey.bind({"ctrl", "shift"}, "c", function()
    +  -- get current space
    +  local currentspace = hs.spaces.focusedspace()
    +  -- get neovide app
    +  local app = hs.application.get("firefox nightly")
    +  -- if app already open:
    +  if app then
    +    -- if no main window, then open a new window
    +    if not app:mainwindow() then
    +      app:selectmenuitem("new os window", true)
    +      -- if app is already in front, then hide it
    +    elseif app:isfrontmost() then
    +      app:hide()
    +      -- if there is a main window somewhere, bring it to current space and to front
    +    else
    +      -- first move the main window to the current space
    +      hs.spaces.movewindowtospace(app:mainwindow(), currentspace)
    +      -- activate the app
    +      app:activate()
    +      -- raise the main window and position correctly
    +      app:mainwindow():raise()
    +    end
    +    -- if app not open, open it
    +  else
    +    hs.application.launchorfocus("firefox nightly")
    +    app = hs.application.get("firefox nightly")
    +  end
    +end)
    ```

--- init.lua
+++ init.lua
@@ -94,3 +94,36 @@ hs.hotkey.bind({"ctrl", "shift"}, "a", function()
     app = hs.application.get("Safari")
   end
 end)
+
+-----------------------------
+--  firefox configuration  --
+-----------------------------
+hs.hotkey.bind({"ctrl", "shift"}, "c", function()
+  -- Get current space
+  local currentSpace = hs.spaces.focusedSpace()
+  -- Get neovide app
+  local app = hs.application.get("Firefox Nightly")
+  -- If app already open:
+  if app then
+    -- If no main window, then open a new window
+    if not app:mainWindow() then
+      app:selectMenuItem("New OS Window", true)
+      -- If app is already in front, then hide it
+    elseif app:isFrontmost() then
+      app:hide()
+      -- If there is a main window somewhere, bring it to current space and to front
+    else
+      -- First move the main window to the current space
+      hs.spaces.moveWindowToSpace(app:mainWindow(), currentSpace)
+      -- Activate the app
+      app:activate()
+      -- Raise the main window and position correctly
+      app:mainWindow():raise()
+    end
+    -- If app not open, open it
+  else
+    hs.application.launchOrFocus("Firefox Nightly")
+    app = hs.application.get("Firefox Nightly")
+  end
+end)
+
