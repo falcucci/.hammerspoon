0fffcf0af488f26bc7a7d7124d7e522cbfd51154
Alexsander Falcucci <alexsander.santos@luizalabs.com>
2023-07-17 17:48:25 +0200

    hotkey: add spotify shortcut using fn-shit-s
    adds a hotkey for spotify to be used when switching between workspaces. it allows the user to open spotify if it is not already running, and to switch between workspaces if it already is. it also hides spotify when it is already in the foreground.
    
    adds a hotkey to the existing `init.lua` file, binding the `ctrl + shift + s` key combination to the relevant action. the code then checks if the app is already running and if not, launches it. if the app is already running, it checks if it is visible in the foreground and if so, hides it, otherwise it brings it back to the current workspace and makes it the foreground app.

--- init.lua
+++ init.lua
@@ -127,3 +127,34 @@ hs.hotkey.bind({"ctrl", "shift"}, "c", function()
   end
 end)
 
+---------------
+--  spotify  --
+---------------
+hs.hotkey.bind({"ctrl", "shift"}, "s", function()
+  -- Get current space
+  local currentSpace = hs.spaces.focusedSpace()
+  -- Get neovide app
+  local app = hs.application.get("Spotify")
+  -- If app already open:
+  if app then
+    -- If no main window, then open a new window
+    if not app:mainWindow() then
+      app:selectMenuItem("New OS Window", true)
+      -- If app is already in front, then hide it
+    elseif app:isFrontmost() then
+      app:hide()
+      -- If there is a main window somewhere, bring it to current space and to front
+    else
+      -- First move the main window to the current space
+      hs.spaces.moveWindowToSpace(app:mainWindow(), currentSpace)
+      -- Activate the app
+      app:activate()
+      -- Raise the main window and position correctly
+      app:mainWindow():raise()
+    end
+    -- If app not open, open it
+  else
+    hs.application.launchOrFocus("Spotify")
+    app = hs.application.get("Spotify")
+  end
+end)
