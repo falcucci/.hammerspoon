7e5c464a0d96bb89d0f30ed1cd7d55fdd60dcd58
Alexsander Falcucci <alex.falcucci@gmail.com>
2024-02-02 01:57:48 +0100

    feat: update hotkey bindings and add new ones
    - changed firefox hotkey binding from `{ "ctrl", "shift" }, "c"` to `{ "rightalt" }, "f"`
    - added hotkey binding for discord with shortcut `{ "rightalt" }, "d"`
    - added hotkey binding for postman with shortcut `{ "rightalt" }, "x"`
    - modified spotify hotkey binding from `{ "ctrl", "shift" }, "s"` to `{ "rightalt" }, "s"`
    - added hotkey binding for telegram with shortcut `{ "rightalt" }, "t"`
    - added hotkey binding for system settings with shortcut `{ "rightalt" }, "c"`
    - added hotkey binding for messages app with shortcut `{ "rightalt" }, "m"`
    all the new hotkeys bring the app to the front, hide it if it's already the foremost, or open a new window if needed.

--- init.lua
+++ init.lua
@@ -2,8 +2,8 @@ local as = require "hs.applescript"
 -------------------------------------
 --  neovide configuration map key  --
 -------------------------------------
-hs.hotkey.bind({"ctrl", "shift"}, "z", function()
-  -- Get current space 
+hs.hotkey.bind({ "ctrl", "shift" }, "z", function()
+  -- Get current space
   local currentSpace = hs.spaces.focusedSpace()
   -- Get neovide app
   local app = hs.application.get("neovide")
@@ -34,8 +34,8 @@ end)
 ----------------------------------
 --  warp configuration map key  --
 ----------------------------------
-hs.hotkey.bind({"ctrl", "shift"}, "x", function()
-  -- Get current space 
+hs.hotkey.bind({ "ctrl", "shift" }, "x", function()
+  -- Get current space
   local currentSpace = hs.spaces.focusedSpace()
   -- Get neovide app
   local app = hs.application.get("warp")
@@ -66,7 +66,7 @@ end)
 ----------------------------
 --  Safari configuration  --
 ----------------------------
-hs.hotkey.bind({"ctrl", "shift"}, "a", function()
+hs.hotkey.bind({ "ctrl", "shift" }, "a", function()
   -- Get current space
   local currentSpace = hs.spaces.focusedSpace()
   -- Get neovide app
@@ -98,7 +98,7 @@ end)
 -----------------------------
 --  firefox configuration  --
 -----------------------------
-hs.hotkey.bind({"ctrl", "shift"}, "c", function()
+hs.hotkey.bind({ "rightalt" }, "f", function()
   -- Get current space
   local currentSpace = hs.spaces.focusedSpace()
   -- Get neovide app
@@ -128,9 +128,75 @@ hs.hotkey.bind({"ctrl", "shift"}, "c", function()
 end)
 
 ---------------
+--  discord  --
+---------------
+hs.hotkey.bind({ "rightalt" }, "d", function()
+  -- Get current space
+  local currentSpace = hs.spaces.focusedSpace()
+  -- Get neovide app
+  local app = hs.application.get("Discord")
+  -- If app already open:
+  if app then
+    -- If no main window, then open a new window
+    if not app:mainWindow() then
+      app:selectMenuItem("New OS Window", true)
+      -- If app is already in front, then hide it
+    elseif app:isFrontmost() then
+      app:hide()
+      -- If there is a main window somewhere, bring it to current space and to front
+    else
+      -- First move the main window to the current space
+      hs.spaces.moveWindowToSpace(app:mainWindow(), currentSpace)
+      -- Activate the app
+      app:activate()
+      -- Raise the main window and position correctly
+      app:mainWindow():raise()
+    end
+    -- If app not open, open it
+  else
+    hs.application.launchOrFocus("Discord")
+    app = hs.application.get("Discord")
+  end
+end)
+
+
+---------------
+--  postman --
+---------------
+hs.hotkey.bind({ "rightalt" }, "x", function()
+  -- Get current space
+  local currentSpace = hs.spaces.focusedSpace()
+  -- Get neovide app
+  local app = hs.application.get("Postman")
+  -- If app already open:
+  if app then
+    -- If no main window, then open a new window
+    if not app:mainWindow() then
+      app:selectMenuItem("New OS Window", true)
+      -- If app is already in front, then hide it
+    elseif app:isFrontmost() then
+      app:hide()
+      -- If there is a main window somewhere, bring it to current space and to front
+    else
+      -- First move the main window to the current space
+      hs.spaces.moveWindowToSpace(app:mainWindow(), currentSpace)
+      -- Activate the app
+      app:activate()
+      -- Raise the main window and position correctly
+      app:mainWindow():raise()
+    end
+    -- If app not open, open it
+  else
+    hs.application.launchOrFocus("Postman")
+    app = hs.application.get("Postman")
+  end
+end)
+
+
+---------------
 --  spotify  --
 ---------------
-hs.hotkey.bind({"ctrl", "shift"}, "s", function()
+hs.hotkey.bind({ "rightalt" }, "s", function()
   -- Get current space
   local currentSpace = hs.spaces.focusedSpace()
   -- Get neovide app
@@ -156,5 +222,102 @@ hs.hotkey.bind({"ctrl", "shift"}, "s", function()
   else
     hs.application.launchOrFocus("Spotify")
     app = hs.application.get("Spotify")
+  end
+end)
+
+----------------
+--  telegram  --
+----------------
+hs.hotkey.bind({ "rightalt" }, "t", function()
+  -- Get current space
+  local currentSpace = hs.spaces.focusedSpace()
+  -- Get neovide app
+  local app = hs.application.get("Telegram")
+  -- If app already open:
+  if app then
+    -- If no main window, then open a new window
+    if not app:mainWindow() then
+      app:selectMenuItem("New OS Window", true)
+      -- If app is already in front, then hide it
+    elseif app:isFrontmost() then
+      app:hide()
+      -- If there is a main window somewhere, bring it to current space and to front
+    else
+      -- First move the main window to the current space
+      hs.spaces.moveWindowToSpace(app:mainWindow(), currentSpace)
+      -- Activate the app
+      app:activate()
+      -- Raise the main window and position correctly
+      app:mainWindow():raise()
+    end
+    -- If app not open, open it
+  else
+    hs.application.launchOrFocus("Telegram")
+    app = hs.application.get("Telegram")
+  end
+end)
+
+
+-----------------------
+--  System Settings  --
+-----------------------
+hs.hotkey.bind({ "rightalt" }, "c", function()
+  -- Get current space
+  local currentSpace = hs.spaces.focusedSpace()
+  -- Get neovide app
+  local app = hs.application.get("System Settings")
+  -- If app already open:
+  if app then
+    -- If no main window, then open a new window
+    if not app:mainWindow() then
+      app:selectMenuItem("New OS Window", true)
+      -- If app is already in front, then hide it
+    elseif app:isFrontmost() then
+      app:hide()
+      -- If there is a main window somewhere, bring it to current space and to front
+    else
+      -- First move the main window to the current space
+      hs.spaces.moveWindowToSpace(app:mainWindow(), currentSpace)
+      -- Activate the app
+      app:activate()
+      -- Raise the main window and position correctly
+      app:mainWindow():raise()
+    end
+    -- If app not open, open it
+  else
+    hs.application.launchOrFocus("System Settings")
+    app = hs.application.get("System Settings")
+  end
+end)
+
+--------------------
+--  Messages app  --
+--------------------
+hs.hotkey.bind({ "rightalt" }, "m", function()
+  -- Get current space
+  local currentSpace = hs.spaces.focusedSpace()
+  -- Get neovide app
+  local app = hs.application.get("Messages")
+  -- If app already open:
+  if app then
+    -- If no main window, then open a new window
+    if not app:mainWindow() then
+      app:selectMenuItem("New OS Window", true)
+      -- If app is already in front, then hide it
+    elseif app:isFrontmost() then
+      app:hide()
+      -- If there is a main window somewhere, bring it to current space and to front
+    else
+      -- First move the main window to the current space
+      hs.spaces.moveWindowToSpace(app:mainWindow(), currentSpace)
+      -- Activate the app
+      app:activate()
+      -- Raise the main window and position correctly
+      app:mainWindow():raise()
+    end
+    -- If app not open, open it
+  else
+    hs.application.launchOrFocus("Messages")
+    app = hs.application.get("Messages")
   end
 end)
